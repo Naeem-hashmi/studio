21fcdfbb00cac552a0d22c6adbd0a5557b89b6bd
Naeem Uddin <naeemuldeen99@gmail.com>
2025-06-16 15:44:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "@/lib/firebase";
